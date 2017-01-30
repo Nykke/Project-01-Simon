cdcf9b588ba4e214db75218434fe46d059b3648b
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-29 20:30:36 -0500

    added javascript pseudo code

---
+++ script.js
@@ -0,0 +1,37 @@
+//create function that changes scoreboard to add 10 points for each successful sequence
+
+//create an event listener for pause button to stop game until pressed again or app is closed
+
+//create an event listener for start button to initialize first sequence when pressed
+
+//create a function that counts down from one minute when pressed
+
+//create a for loop that adds 5 seconds to every 5 levels achieved as extra time to complete round
+
+//create a side feature that turns timer off
+
+//create an if statement that states when start button is hit that a random sequence will start
+
+//create an event listener that waits for user to hit a mirror sequence before running again
+
+//prevent page from refreshing
+
+//create a do while loop that waits and runs while user hits repeated sequence
+
+//create a switch statement that adds one more random sequence to each new level
+
+//create a for loop that runs within each level with new sequences
+
+//create an event listener that waits for a sucessful completion of level to rotate petals before transition to next level
+
+//create an if, elseif, else statement that says 'if user hits incorrect sequence, game alerts a "wrong buzz" and ends'.
+
+//create an alert asking user if they will like to pay another game after losing
+
+//create an event listener for the alert that says 'yes' or 'no'
+
+//create an if, else statement that says 'if user clicks "yes" previous level will restart, if "no", box will close and go back to "home".'
+
+//create a function that stores scores
+
+//game will remember levels achieved and let user to continue where they left off
