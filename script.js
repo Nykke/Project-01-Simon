954e2939bff860e292a61df563ce1738e119cbc3
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-16 13:29:34 -0500

    JS pseudo code

---
+++ script.js
@@ -0,0 +1,17 @@
+//BRONZE
+//player should be able to hit the start button ONLY to start Simon game
+//once the game has started user should be able to see one random petal light up
+//after the random petal lights up, user should be able to click on the same petal triggering a response
+//responses: 1) same pattern, the game continues; 2) incorrect pattern, the game ends
+//for each correct response the game should be able to repeat the previous correct pattern with AN ADDITIONAL pattern/color
+//player should be able to click on pause button to pause game
+//player should be able to click twice to continue game
+//upon completion of level, the game should restart
+
+//SILVER
+//the game should add a score of "5 points" for each correct pattern logged
+//the game should add "20 points" for a successful completion of level
+//the player should be able to see a timer
+//player has a set time to complete each level
+//petals of flowers should rotate when level is complete
+//diplay tells player what their highest score was
