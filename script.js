8109d85b8a508018a607416d5f27892e8a71aa18
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-18 23:59:17 -0400

    audio and functions added

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ $(document).ready(function(){
 //test console
   // console.log($);
 
-//BRONZE
+  //audio
+  var audio = [];
+  audio[1] = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+  audio[2] = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+  audio[3] = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+  audio[4] = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+  var audioIncorrect = new Audio("https://www.dropbox.com/s/ud4fafa7uifk7ah/loseSimon.mp3?dl=1");
+
+  //set game and player arrays
+  var simonSequence = [];
+  var playerSequence = [];
+
+  //set timer
+  var seconds = 0;
+  var clockId;
+
   //player should be able to hit the start button ONLY to start Simon game
   //once the game has started user should be able to see one random petal light up
 
@@ -10,43 +25,69 @@ $(document).ready(function(){
   $("#start").click(function (){
       console.log("simon is on");
 
+    //timer
+    function updateClock(){
+      seconds++;
+      $("#clock").html(seconds);
+    }
+
+    clockId = setInterval(updateClock, 1000);
+
+      simonSequence = [];
+      playerSequence = [];
+      addNextPetal();
+      petalSequence();
+
   //random petal selection sequence
     var sequenceArray = [0, 1, 2, 3];
       console.log("sequence is up");
-    var randomNum = sequenceArray[Math.floor(Math.random() * 4)];
+    var randomNum = sequenceArray[Math.floor(Math.random() * 5)];
     switch(randomNum){
       case 0:
         $("#red").fadeOut(1000).fadeIn(1000);
+        audio[1].play();
         break;
       case 1:
         $("#blue").fadeOut(1000).fadeIn(1000);
+        audio[2].play();
         break;
       case 2:
         $("#green").fadeOut(1000).fadeIn(1000);
+        audio[3].play();
         break;
       case 3:
         $("#yellow").fadeOut(1000).fadeIn(1000);
+        audio[4].play();
         break;
       default:
         break;
    }
 
-    });
+ });
 
-  //after the random petal lights up, user should be able to click on the same petal triggering a response
-  //responses: 1) same pattern, the game continues; 2) incorrect pattern, the game ends
-  petals.each(["red", "blue", "green", "yellow"], function (clickedPetal){
+ //after the random petal lights up, user should be able to click on the same petal triggering a response
+ //responses: 1) same pattern, the game continues; 2) incorrect pattern, the game ends
+ function continueGame() {
+   if (simonSequence.length == playerSequence.length) {
+     addNextPetal();
+     setTimeOut(petalSequence, 2000);
+   } else {
+       alert("INCORRECT, GAME OVER!");
+       audioIncorrect.play();
+   }
 
+   function addNextPetal() {
+     simonSequence.push(randomNum);
+   }
 
-  });
-  var clickedPetal = [];
-    var simon = function play() {
-      var times = 12;
-        for (i = 0; i < 12; i++){
-          return play;
-        }
-      };
-       petals.push(clickedPetal);
+ //for each correct response the game should be able to repeat the previous correct pattern with AN ADDITIONAL pattern/color
+   function petalSequence(){
+     for (var i = 0; i < simonSequence.length; i++){
+
+     }
+
+   }
+ }
 
 
 
@@ -55,17 +96,7 @@ $(document).ready(function(){
 }); //end of document ready
 
 
-//after the random petal lights up, user should be able to click on the same petal triggering a response
-//responses: 1) same pattern, the game continues; 2) incorrect pattern, the game ends
-//for each correct response the game should be able to repeat the previous correct pattern with AN ADDITIONAL pattern/color
+
 //player should be able to click on pause button to pause game
 //player should be able to click twice to continue game
 //upon completion of level, the game should restart
-
-//SILVER
-//the game should add a score of "5 points" for each correct pattern logged
-//the game should add "20 points" for a successful completion of level
-//the player should be able to see a timer
-//player has a set time to complete each level
-//petals of flowers should rotate when level is complete
-//diplay tells player what their highest score was
